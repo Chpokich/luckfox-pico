ada4958b243e34c6e010808dc5a56d660fa15816
Chpokich <goggomon@gmail.com>
2026-03-11 10:59:08 +0300

    run.sh patch

--- run.sh
+++ run.sh
@@ -9,15 +9,10 @@ elif [ "$1" == "2" ]; then
   sudo upgrade_tool uf $CURRENT_DIR/output/image/update.img
 elif [ "$1" == "3" ]; then 
   scp $CURRENT_DIR/output/out/oem/usr/ko/sc3336.ko root@172.32.0.93:/root/$2
-elif [ "$1" == "$NULL" ]; then 
+elif [ -z "$1" ]; then 
   echo "ERROR: put the first param! 
     select one of the following: 
     1 -- build 
     2 -- emmc flash 
     3 -- copy sc3336.ko to luckfox"
 fi
-
-#cp /home/sysdrv/source/buildroot/buildroot-2023.02.6/.config /home/sysdrv/source/buildroot/buildroot-2023.02.6/configs/luckfox_pico_w_defconfig
-#rm -f output/image/update.img
-#./build.sh
-#sudo upgrade_tool uf /home/zelentcov/luckfox/luckfox-pico/output/image/update.img
